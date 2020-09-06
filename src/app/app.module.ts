e15e5363df85141c3c80e21ad4c4f5ec9731e09c
Sovam-coder <sovam121@gmail.com>
2020-09-06 22:53:01 +0530

    add angular material to repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatRadioModule,
+
+    MatInputModule,
+
     AppRoutingModule
   ],
   providers: [],
